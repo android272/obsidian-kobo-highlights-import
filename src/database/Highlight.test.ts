2561c272fbe34f7a8672399edfdd66fca9d0e1ac
Andrew Pullins <apullins@mindforgestudios.com>
2022-12-18 21:41:16 -0500

    Adding tests for multiple content back in.

--- src/database/Highlight.test.ts
+++ src/database/Highlight.test.ts
@@ -333,4 +333,121 @@ describe('HighlightService', async function () {
             })
         })
     })
+
+    describe('with multiple content', async function() {
+
+        const contentMap = new Map<string, Content>([
+            [
+                "e7f8f92d-38ca-4556-bab8-a4d902e9c430",
+                {
+                    title: "Chapter Eight: Holden",
+                    contentId: "file:///mnt/onboard/Corey, James S.A_/Nemesis Games - James S.A. Corey.epub#(12)OEBPS/Text/ch09.html",
+                    bookTitle: "Nemesis Games",
+                    chapterIdBookmarked: "e7f8f92d-38ca-4556-bab8-a4d902e9c430!Text/chapter002.xhtml"
+                }
+            ],
+            [
+                "d40c9071-993f-4f1f-ae53-594847d9fd27",
+                {
+                    title: "On Passwords and Power Drills",
+                    contentId: "/mnt/onboard/Adkins, Heather & Beyer, Betsy & Blankiotr & Oprea, Ana & Stubblefield, Adam/Building Secure and Reliable Systems_ Best PractiLewandowski & Ana Oprea & Adam Stubblefield.kepub.epub!!OEBPS/ch01.html#on_passwords_and_power_drills-2",
+                    bookTitle: "Building Secure and Reliable Systems: Best Practices for Designing, Implementing, and Maintaining Systems",
+                    chapterIdBookmarked: "/mnt/onboard/Adkins, Heather & Beyer, Betsy & Blankiotr & Oprea, Ana & Stubblefield, Adam/Building Secure and Reliable Systems_ Best PractiLewandowski & Ana Oprea & Adam Stubblefield.kepub.epub!!OEBPS/ch01.html"
+                }
+            ],
+            [
+                "3408844d-65a6-4d23-9d99-8f189ca07d0b",
+                {
+                    title: "Dune",
+                    contentId: "23ba3dcf-3543-476c-984b-2f746c859763!OEBPS!Text/chapter001.xhtml-1",
+                    bookTitle: "Dune",
+                    chapterIdBookmarked: "23ba3dcf-3543-476c-984b-2f746c859763!OEBPS!Text/chapter001.xhtml"
+                }
+            ],
+            [
+                "c0d92aca-e4bb-476a-8131-ee0c0c21ced5",
+                {
+                    title: "11. Being On-Call",
+                    contentId: "bce81485-5e92-4cca-8965-613c3ca12737!OEBPS!ch11.html#chapter_oncall-engineer-1",
+                    bookTitle: "Site Reliability Engineering",
+                    chapterIdBookmarked: "bce81485-5e92-4cca-8965-613c3ca12737!OEBPS!ch11.html"
+                }
+            ]
+        ])
+
+        const bookmarkMap = new Map<string, Bookmark>([
+            [
+                "e7f8f92d-38ca-4556-bab8-a4d902e9c430",
+                <Bookmark> {
+                    text: "“I guess I can’t be. How do you prove a negative?”",
+                    contentId: "e7f8f92d-38ca-4556-bab8-a4d902e9c430",
+                    note: '',
+                    dateCreated: new Date('2022-08-05T20:46:41+00:00')
+                }
+            ],
+            [
+                "d40c9071-993f-4f1f-ae53-594847d9fd27",
+                <Bookmark> {
+                    text: "“I guess I can’t be. How do you prove a negative?”",
+                    contentId: "d40c9071-993f-4f1f-ae53-594847d9fd27",
+                    note: '',
+                    dateCreated: new Date('2022-08-05T20:46:41+00:00')
+                }
+            ],
+            [
+                "3408844d-65a6-4d23-9d99-8f189ca07d0b",
+                <Bookmark> {
+                    text: "“I guess I can’t be. How do you prove a negative?”",
+                    contentId: "3408844d-65a6-4d23-9d99-8f189ca07d0b",
+                    note: '',
+                    dateCreated: new Date('2022-08-05T20:46:41+00:00')
+                }
+            ],
+            [
+                "c0d92aca-e4bb-476a-8131-ee0c0c21ced5",
+                <Bookmark> {
+                    text: "“I guess I can’t be. How do you prove a negative?”",
+                    contentId: "c0d92aca-e4bb-476a-8131-ee0c0c21ced5",
+                    note: '',
+                    dateCreated: new Date('2022-08-05T20:46:41+00:00')
+                }
+            ],
+        ])
+
+        let repo: Repository
+        let service: HighlightServiceCopy
+
+        before(async function () {
+            repo = <Repository> {}
+            repo.getContentByContentId = (contentId) => Promise.resolve(contentMap.get(contentId) ?? null)
+            repo.getTotalBookmark = () => Promise.resolve(contentMap.size);
+            const bookmarks = new Array<Bookmark>()
+            bookmarkMap.forEach(entry => bookmarks.push(entry))
+            repo.getAllBookmark = () => Promise.resolve(bookmarks);
+            repo.getBookmarkById = (bookmarkId) => Promise.resolve(bookmarkMap.get(bookmarkId) ?? null)
+            service = new HighlightServiceCopy(repo)
+        })
+
+        it('getAllHighlight', async function () {
+            const all = await service.getAllHighlight()
+            const total = await repo.getTotalBookmark()
+            chai.expect(all).length(total)
+        })
+
+        for (const [id, content] of contentMap) {
+            it(`createHilightFromBookmark ${id}`, async function () {
+                const bookmark = await repo.getBookmarkById(id)
+                if (!bookmark) {
+                    chai.assert.isNotNull(bookmark)
+                    return
+                }
+
+                const highlight = await service.createHilightFromBookmark(bookmark)
+                chai.assert.deepEqual(highlight, {
+                    content: content,
+                    bookmark: bookmark
+                })
+            })
+        }
+    })
 });
